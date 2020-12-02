8949c33282e4c38d42d90501af7ba3ce9cbbd961
Dean Krotzer <krotzersoftwaredevelopment@gmail.com>
2020-12-01 20:44:22 -0800

    Updates

---
+++ precache-manifest.ba2a1f32eae1cc82fcb4b818354ee8b3.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f61ef8ca3323327c18e066b83f24e31",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d130651b62522cd03721",
+    "url": "/static/css/2.30c2d978.chunk.css"
+  },
+  {
+    "revision": "05ab25dc6388a01e221b",
+    "url": "/static/css/main.193b53e5.chunk.css"
+  },
+  {
+    "revision": "d130651b62522cd03721",
+    "url": "/static/js/2.2cffb703.chunk.js"
+  },
+  {
+    "revision": "14060d5403b5cb599a71704f7b391b06",
+    "url": "/static/js/2.2cffb703.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05ab25dc6388a01e221b",
+    "url": "/static/js/main.a14e9ef0.chunk.js"
+  },
+  {
+    "revision": "8e4b18004409f9f25919",
+    "url": "/static/js/runtime-main.f15af103.js"
+  },
+  {
+    "revision": "5314dab9b932b65cc40c86ef69aff264",
+    "url": "/static/media/Aclonica-Regular.5314dab9.ttf"
+  },
+  {
+    "revision": "22c595c04db22bb0363d711f5c1cd486",
+    "url": "/static/media/Alatsi-Regular.22c595c0.ttf"
+  },
+  {
+    "revision": "c782c6d5122b3c60fd74ebd8289de0f0",
+    "url": "/static/media/Switch1.c782c6d5.png"
+  },
+  {
+    "revision": "662301d790199edaf7cd3462157c7d6b",
+    "url": "/static/media/bg.662301d7.png"
+  }
+]);
